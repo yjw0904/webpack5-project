a45eee478771818c0b97b07752f8d891b0efaa0b
yjw0904 <yjw0904@gmail.com>
2021-03-24 21:37:52 -0400

    Add babel to project

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenShieldRecipe = {
   refinedMoonstone: 4, 
 }
 
+const elvenGauntletsRecipee ={
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonstone: 4,
+}
+
 console.log(elvenShieldRecipe)
+console.log(elvenGauntletsRecipee)
