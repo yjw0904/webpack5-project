c5981671feb8a80a6aed4e4f20afad4547e1fdb8
yjw0904 <yjw0904@gmail.com>
2021-03-28 19:33:18 -0400

    Adding eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: { indent: ['error', 2] },
+  settings: { version: 'detect' },
+}
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "es2021": true
+//     },
+//     "extends": [
+//         "eslint:recommended",
+//         "plugin:react/recommended"
+//     ],
+//     "parserOptions": {
+//         "ecmaFeatures": {
+//             "jsx": true
+//         },
+//         "ecmaVersion": 12,
+//         "sourceType": "module"
+//     },
+//     "plugins": [
+//         "react"
+//     ],
+//     "rules": {
+//     }
+// };
