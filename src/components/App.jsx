e32edc71c1e47196530bbbc842a36457d1e107c0
yjw0904 <yjw0904@gmail.com>
2021-04-15 22:52:17 -0400

    Added dispatch call to saga

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import Grid from '@material-ui/core/Grid'
 import Container from '@material-ui/core/Container'
 
 import { Tile } from './Tile'
 import { FullSizeLineChart } from './FullSizeLineChart'
 
+import { fetchHomePageData } from '../sagas/home_page/actions'
+
 export const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchHomePageData())    
+  },[])
+
   return (
     <Container maxWidth='lg'>
       <Grid 
