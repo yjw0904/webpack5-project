5e63c2c12b7d43649d2e7a21fcf51fb19c7a3d50
yjw0904 <yjw0904@gmail.com>
2021-04-19 03:23:29 -0400

    Added circualr progress for the loading time

--- src/components/Tile/index.jsx
+++ src/components/Tile/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import { useSelector } from 'react-redux'
+// Mui components
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
-
+// Components
 import { useStyles } from './styles'
-
+/**
+ * Generic tile component
+ */
 export const Tile = ({ children, title }) => {
   const classes = useStyles()
-
+  const dataIsLoading = useSelector((state) => state.app.dataIsLoading)
   return (
     <Paper className={classes.paperContainer}>
       {title && (
@@ -19,12 +24,23 @@ export const Tile = ({ children, title }) => {
           </Typography>
         </Container>
       )}
-      <Container disableGutters>{children}</Container>
-    </Paper>    
-  )  
+      {dataIsLoading ? (
+        <Grid container className={classes.circularProgress} align='center'>
+          <Grid item xs={12}>
+            <CircularProgress
+              size={90}
+              color='primary'
+              className={classes.circularProgress}
+            />
+          </Grid>
+        </Grid>
+      ) : (
+        <Container disableGutters>{children}</Container>
+      )}
+    </Paper>
+  )
 }
-
 Tile.propTypes = {
-  title: PropTypes.string,  
+  title: PropTypes.string,
   children: PropTypes.element,
 }
