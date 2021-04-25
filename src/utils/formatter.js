495e7ade9699fa71827dd5ec3b9f8f7c5452fba2
yjw0904 <yjw0904@gmail.com>
2021-04-24 21:29:23 -0400

    Added two new formatters

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -1,8 +1,17 @@
 import numbro from 'numbro'
+import moment from 'moment'
 
 export const formatNumberWholePercentage = (value) => {
   return numbro(value).format({
     output: 'percent',
     mantissa: 0,
   })
+}
+
+export const formatNumberNoDecimal = (value) => {
+  return numbro(value).format({ thousandSeparated: true })
+}
+
+export const formatDateMMMDYYYY = (timeStamp) => {
+  return moment(timeStamp, 'YYYY-MM-DDTHH:mm:ss').format('MMM D, YYYY')
 }
